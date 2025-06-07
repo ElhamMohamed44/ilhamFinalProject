4aa4274112c6c1d4d6150193d364667e92534266
ElhamMohamed44 <elham.orabi22d@eslsca.edu.eg>
2025-06-07 03:52:27 +0300

    adding color wheels&colorbodywithglasses color

--- Assets/TapToPlaceCar.cs
+++ Assets/TapToPlaceCar.cs
@@ -1,56 +1,88 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
-using UnityEngine.EventSystems;
 
 public class TapToPlaceCar : MonoBehaviour
 {
-    public GameObject[] carPrefabs;
-    public static GameObject activeCar1;
-    public static GameObject activeCar2;
-    private int currentCarIndex = 0;
+    public GameObject car1Prefab;
+    public GameObject car2Prefab;
+    public ARRaycastManager raycastManager;
+    public Camera arCamera;
 
-    private ARRaycastManager raycastManager;
-    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    public Material[] bodyColors;
+    public Material[] wheelColors;
 
-    void Awake()
-    {
-        raycastManager = GetComponent<ARRaycastManager>();
-    }
+    private GameObject selectedPrefab;
+    private GameObject spawnedCar;
+
+    private int bodyIndex = 0;
+    private int wheelIndex = 0;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    public void SelectCar1() { selectedPrefab = car1Prefab; Debug.Log("Car 1 Selected"); }
+    public void SelectCar2() { selectedPrefab = car2Prefab; Debug.Log("Car 2 Selected"); }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
-        {
-            Vector2 screenPos = Input.mousePosition;
+#if UNITY_EDITOR
+        if (selectedPrefab == null) return;
 
-            if (raycastManager.Raycast(screenPos, hits, TrackableType.PlaneWithinPolygon))
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 fakePos = arCamera.transform.position + arCamera.transform.forward * 2;
+            Pose fakePose = new Pose(fakePos, Quaternion.identity);
+
+            if (spawnedCar != null)
+                Destroy(spawnedCar);
+
+            spawnedCar = Instantiate(selectedPrefab, fakePose.position, fakePose.rotation);
+            ApplyCustomization();
+        }
+#else
+        if (selectedPrefab == null) return;
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Vector2 touchPos = Input.GetTouch(0).position;
+
+            if (raycastManager.Raycast(touchPos, hits, TrackableType.PlaneWithinPolygon))
             {
                 Pose hitPose = hits[0].pose;
 
-                if (currentCarIndex == 0)
-                {
-                    if (activeCar1 != null)
-                        Destroy(activeCar1);
+                if (spawnedCar != null)
+                    Destroy(spawnedCar);
 
-                    activeCar1 = Instantiate(carPrefabs[0], hitPose.position, Quaternion.identity);
-                    activeCar1.transform.localScale = Vector3.one * 0.2f;
-                    Debug.Log("✅ Car 1 placed at: " + hitPose.position);
-                }
-                else if (currentCarIndex == 1)
-                {
-                    if (activeCar2 != null)
-                        Destroy(activeCar2);
-
-                    activeCar2 = Instantiate(carPrefabs[1], hitPose.position, Quaternion.identity);
-                    activeCar2.transform.localScale = Vector3.one * 0.2f;
-                    Debug.Log("✅ Car 2 placed at: " + hitPose.position);
-                }
+                spawnedCar = Instantiate(selectedPrefab, hitPose.position, hitPose.rotation);
+                ApplyCustomization();
             }
         }
+#endif
     }
 
-    public void SelectCar0() { currentCarIndex = 0; }
-    public void SelectCar1() { currentCarIndex = 1; }
+    public void ChangeBodyColor()
+    {
+        if (spawnedCar == null) return;
+        bodyIndex = (bodyIndex + 1) % bodyColors.Length;
+        ApplyCustomization();
+    }
+
+    public void ChangeWheelColor()
+    {
+        if (spawnedCar == null) return;
+        wheelIndex = (wheelIndex + 1) % wheelColors.Length;
+        ApplyCustomization();
+    }
+
+    void ApplyCustomization()
+    {
+        Renderer[] renderers = spawnedCar.GetComponentsInChildren<Renderer>();
+        foreach (var rend in renderers)
+        {
+            if (rend.name.ToLower().Contains("wheel"))
+                rend.material = wheelColors[wheelIndex];
+            else
+                rend.material = bodyColors[bodyIndex];
+        }
+    }
 }
